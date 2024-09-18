36ca2255dc9877507448e48297749303a03e2f4a
Fernandamenass <0243868@up.edu.mx>
2024-09-17 20:57:49 -0600

    xx

--- Tutorials/04_Simon/simon.js
+++ Tutorials/04_Simon/simon.js
@@ -1,14 +1,13 @@
 //Cuando se llama con punto es una clase, cuendo se llama con # es un id
-// Variables globales
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
 let started = false;
 
-// Función para generar una secuencia aleatoria y mostrar toda la secuencia hasta el nivel actual
+// Función de secuencia aleatoria
 function nextSequence() {
-  userClickedPattern = []; // Reinicia el patrón del usuario para el nuevo nivel
+  userClickedPattern = []; 
   level++;
   $("#level-title").text("Level " + level);
 
@@ -16,28 +15,27 @@ function nextSequence() {
   const randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  // Mostrar toda la secuencia acumulada
   showSequence();
 }
 
-// Función para mostrar la secuencia completa hasta el nivel actual
+// Secuencia completa
 function showSequence() {
-  let delay = 0; // Variable para aumentar el retraso en cada paso
+  let delay = 0;
 
-  // Recorrer el patrón del juego y mostrar cada color en secuencia
+  // Mostrar los patrones
   for (let i = 0; i < gamePattern.length; i++) {
     setTimeout(function () {
       const color = gamePattern[i];
-      $("#" + color).fadeOut(100).fadeIn(100); // Efecto de parpadeo
-      playSound(color); // Reproducir el sonido del botón
+      $("#" + color).fadeOut(100).fadeIn(100); // Efecto de prender y apagar
+      playSound(color);
     }, delay);
 
-    delay += 600; // Incrementar el tiempo de retraso entre cada color
+    delay += 600; // Tiempo de retraso entre cada color
   }
 }
 
 $(document).ready(function () {
-  // Listener para detectar la pulsación de una tecla para comenzar el juego
+  //Detectar cualquier tecla para iniciar el juego
   $(document).keypress(function () {
     if (!started) {
       $("#level-title").text("Level " + level);
@@ -46,7 +44,7 @@ $(document).ready(function () {
     }
   });
 
-  // Listener para el evento click en los botones
+  // Click en los botones
   $(".btn").click(function () {
     const userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
@@ -54,28 +52,26 @@ $(document).ready(function () {
     playSound(userChosenColor);
     animatePress(userChosenColor);
 
-    // Verificar la respuesta después de cada clic
-    checkAnswer(userClickedPattern.length - 1); // Envía el índice del último clic
+    // Verificar la respuesta
+    checkAnswer(userClickedPattern.length - 1); // Envía el último clic
   });
 
-  // Función para verificar la respuesta del usuario
+  // Verificar la respuesta
   function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       console.log("Success");
 
-      // Verificar si el usuario ha terminado de ingresar la secuencia completa
+      // Ver si el usuario termino
       if (userClickedPattern.length === gamePattern.length) {
         setTimeout(function () {
-          nextSequence(); // Genera la siguiente secuencia
+          nextSequence(); // Hace la siguiente secuencia
         }, 1000);
       }
     } else {
       console.log("Wrong");
 
-      // Reproducir sonido "wrong"
       playSound("wrong");
 
-      // Aplicar el estilo de "game-over" y cambiar el título
       $("body").addClass("game-over");
       $("#level-title").text("Game Over, Press Any Key to Restart");
 
@@ -83,18 +79,17 @@ $(document).ready(function () {
         $("body").removeClass("game-over");
       }, 200);
 
-      // Llamar a la función para reiniciar el juego
       startOver();
     }
   }
 
-  // Función para reproducir sonido según el color
+  // Sonido segun el color
   function playSound(name) {
     const audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
   }
 
-  // Función para animar el botón presionado
+  // Animar el botón presionado
   function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
     setTimeout(function () {
@@ -102,10 +97,10 @@ $(document).ready(function () {
     }, 100);
   }
 
-  // Función para reiniciar el juego
+  // Reiniciar el juego
   function startOver() {
-    level = 0; // Reiniciar nivel
-    gamePattern = []; // Vaciar el patrón del juego
-    started = false; // Reiniciar el estado del juego
+    level = 0; 
+    gamePattern = [];
+    started = false;
   }
 });
