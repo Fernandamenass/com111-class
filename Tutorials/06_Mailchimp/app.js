1d0aa846dd51ae5fd6920ad089a0326dd4a8963b
Fernandamenass <0243868@up.edu.mx>
2024-12-02 18:13:09 -0600

    No lo pude acabar...

--- Tutorials/06_Mailchimp/app.js
+++ Tutorials/06_Mailchimp/app.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
+// Middleware setup
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
+  console.log(firstName, lastName, email);
+  res.sendFile(__dirname + "/success.html");
+});
 
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
 
-
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
